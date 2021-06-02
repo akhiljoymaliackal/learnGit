91e6f292e92e6d5a257892350898a9a4a2312dec
Akhil Joy <akhilmaliackal@gmail.com>
2021-06-02 07:50:34 +0530

    dev1_1 deleted

--- ETL/app.py
+++ ETL/app.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 dev added line 1
-dev 1 added line 3
-=======
-dev 1 added line 1
 dev 2 added line 2
->>>>>>> 0b0a08f73022a1be550721159cac3575ed2bf4f3
+dev 1 added line 3
+
--- ETL/app_BASE_43731.py
+++
@@ -1,1 +0,0 @@
-dev added line 1
